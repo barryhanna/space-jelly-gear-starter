86850300953904d8393896543bc99ba0266aae45
Barry Hanna <barryhanna@users.noreply.github.com>
2022-04-15 13:11:27 +0100

    Adding dynamic product pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ export default function Home({ home, products }) {
           {products.map((product) => {
             return (
               <li key={product.slug}>
-                <Link href="#">
+                <Link href={`/products/${product.slug}`}>
                   <a>
                     <div className={styles.productImage}>
                       <img
