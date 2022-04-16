4e3726ef63f6a5bd3f6b46ba085bb1fb18ccfded
Barry Hanna <barryhanna@users.noreply.github.com>
2022-04-16 09:37:59 +0100

    Added Snipcart

--- src/pages/products/[productSlug].js
+++ src/pages/products/[productSlug].js
@@ -36,7 +36,16 @@ export default function Product({ product }) {
             />
             <p className={styles.productPrice}>${product.price}</p>
             <p className={styles.productBuy}>
-              <Button>Add to Cart</Button>
+              <Button
+                className="snipcart-add-item"
+                data-item-id={product.id}
+                data-item-price={product.price}
+                data-item-url={`/products/${product.slug}`}
+                data-item-image={product.image.url}
+                data-item-name={product.name}
+              >
+                Add to Cart
+              </Button>
             </p>
           </div>
         </div>
@@ -61,6 +70,7 @@ export async function getStaticProps({ params }) {
           description {
             html
           }
+          slug
         }
       }
     `,
